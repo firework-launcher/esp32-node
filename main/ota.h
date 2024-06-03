acc09a2276f49f8ce25aa57a259b69cb8dfc2e9d
lukeg007 <luke@gilsinger.net>
2024-06-03 00:02:16 -0400

    Remove /write_flash

--- main/ota.h
+++ main/ota.h
@@ -254,54 +254,6 @@ esp_err_t update_post_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
-esp_err_t write_flash_post_handler(httpd_req_t *req)
-{
-    char buf[1000];
-    esp_ota_handle_t ota_handle;
-    int remaining = req->content_len;
-
-    size_t offset = 0x8000;
-    
-
-    const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
-    ESP_ERROR_CHECK(esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle));
-
-    while (remaining > 0) {
-        int recv_len = httpd_req_recv(req, buf, MIN(remaining, sizeof(buf)));
-
-        // Timeout Error: Just retry
-        if (recv_len == HTTPD_SOCK_ERR_TIMEOUT) {
-            continue;
-
-        // Serious Error: Abort OTA
-        } else if (recv_len <= 0) {
-            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Protocol Error");
-            return ESP_FAIL;
-        }
-
-        // Successful Upload: Flash firmware chunk
-        if (esp_ota_write(ota_handle, (const void *)buf, recv_len) != ESP_OK) {
-            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Flash Error");
-            return ESP_FAIL;
-        }
-
-        remaining -= recv_len;
-    }
-
-    // Validate and switch to new OTA image and reboot
-    if (esp_ota_end(ota_handle) != ESP_OK || esp_ota_set_boot_partition(ota_partition) != ESP_OK) {
-            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Validation / Activation Error");
-            return ESP_FAIL;
-    }
-
-    httpd_resp_sendstr(req, "Firmware update complete, rebooting now!\n");
-
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-    esp_restart();
-
-    return ESP_OK;
-}
-
 /*
  * HTTP Server
  */
@@ -339,13 +291,6 @@ httpd_uri_t update_post = {
     .uri      = "/update",
     .method   = HTTP_POST,
     .handler  = update_post_handler,
-    .user_ctx = NULL
-};
-
-httpd_uri_t write_flash_post = {
-    .uri      = "/write_flash",
-    .method   = HTTP_POST,
-    .handler  = write_flash_post_handler,
     .user_ctx = NULL
 };
 
@@ -461,7 +406,6 @@ void ota(pthread_attr_t attr) {
         ws2811_set_all(20, 20, 0);
         ws2811_set_leds();
         httpd_register_uri_handler(http_server, &update_post);
-        httpd_register_uri_handler(http_server, &write_flash_post);
 
         const esp_partition_t *partition = esp_ota_get_running_partition();
         printf("Currently running partition: %s\r\n", partition->label);
